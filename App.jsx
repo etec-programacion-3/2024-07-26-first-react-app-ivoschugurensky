ff7ecc450cccc69a1e962ce934ac7e1a5da8c66d
ivoschugurensky <107146836+ivoschugurensky@users.noreply.github.com>
2024-08-07 13:02:31 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import HolaMundo from "./components/HolaMundo"
+function App() {
+  return(
+      <div>
+        <HolaMundo />,
+        <p>Programacion 3</p>
+      </div>
+  )
+  
+}
+export default App  
