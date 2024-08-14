dc28d9f4d3b81fe4f7a4f97353641d3f9ada5bab
ivoschugurensky <i.schugurensky@alumno.etec.um.edu.ar>
2024-08-14 12:36:40 -0300

    14/08

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+function Header (){
+    return(
+        <div>
+            
+            <header className="header">
+            <h1 className="logo">Mi Logo</h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Inicio</a></li>
+                    <li><a href="#about">Sobre Nosotros</a></li>
+                    <li><a href="#services">Servicios</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        </div>
+        
+        
+
+    )
+}
+
+export default Header;
